ee478215afe8cfe99342e32ec7ab74d51fb1c7d5
mrlth <mrlth@qq.com>
2021-01-21 09:38:36 +0800

    20.有效的括号 string.replace

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -2,7 +2,7 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-01-21 08:30:49
- * @LastEditTime: 2021-01-21 08:55:44
+ * @LastEditTime: 2021-01-21 09:32:18
  * @Description: file content
  */
 /*
@@ -17,6 +17,8 @@
  * @return {boolean}
  */
 var isValid = function (s) {
+  //#region solution 1
+  /**
   if (s === '') return true
 
   const stack = []
@@ -26,8 +28,8 @@ var isValid = function (s) {
   dict[']'] = '['
   dict['}'] = '{'
 
-  for (const c of s) {
-
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i]
     const top = stack[stack.length - 1]
 
     if (startList.includes(c))
@@ -41,6 +43,55 @@ var isValid = function (s) {
   }
 
   return !stack.length
+  /**/
+  //#endregion
+
+  //#region solution 2
+  /** *
+  if (s === '') return true
+
+  const stack = []
+
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i]
+    const top = stack[stack.length - 1]
+
+    switch (c) {
+      case '(':
+        stack.push(')')
+        break
+      case '[':
+        stack.push(']')
+        break
+      case '{':
+        stack.push('}')
+        break
+      default:
+        if (c !== top)
+          return false
+        stack.pop()
+    }
+
+  }
+
+  return !stack.length
+  /** */
+  //#endregion
+
+  //#region solution 3
+  let has = false
+  while (
+    s.indexOf('()') !== -1 ||
+    s.indexOf('[]') !== -1 ||
+    s.indexOf('{}') !== -1
+  ) {
+
+    s = s.replace('()', '')
+    s = s.replace('[]', '')
+    s = s.replace('{}', '')
+  }
+  return !s.length
+  //#endregion
 };
 // @lc code=end
 
