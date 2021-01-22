9ae5730238cd2353bea1c787694e67c10692295c
mrlth <mrlth@qq.com>
2021-01-22 11:25:12 +0800

    239.滑动窗口最大值

--- 239.滑动窗口最大值.js
+++ 239.滑动窗口最大值.js
@@ -2,7 +2,7 @@
  * @Author: mrlthf11
  * @LastEditors: mrlthf11
  * @Date: 2021-01-21 15:58:10
- * @LastEditTime: 2021-01-22 11:16:29
+ * @LastEditTime: 2021-01-22 11:22:35
  * @Description: file content
  */
 /*
@@ -25,25 +25,22 @@ var maxSlidingWindow = function (nums, k) {
   const queue = [], rst = [], len = nums.length;
 
   for (let l = 1 - k, r = 0; r < len; l++, r++) {
-
-    if (l > 0 && queue[0][1] < l) {
-      queue.shift()
-    }
-
     const val = nums[r]
 
     while (queue.length && queue[queue.length - 1][0] <= val) {
       queue.pop()
     }
 
-
     queue.push([val, r])
+
+    if (queue[0][1] < l) {
+      queue.shift()
+    }
 
     if (l >= 0) {
       rst.push(queue[0][0])
     }
 
-    // debugger
   }
 
   return rst
